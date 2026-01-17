af8d7568098363c81f51625bff0acc6665dd3fa8
Alcsel <69143252+alcsel@users.noreply.github.com>
2026-01-17 16:06:41 +0300

    Update struct.cpp

--- january/struct.cpp
+++ january/struct.cpp
@@ -9,11 +9,12 @@ struct student  {
 };
 int main(){
     student student1;
-    student1.name = "selim";
-    student1.age = 19;
-    student1.grade = '2';
+    student1.name = "(name)";
+    student1.age = 20;
+    student1.grade = '56';
     cout<<"Name: "<<student1.name<<endl;
     cout<<"Age: "<<student1.age<<endl;
     cout<<"Grade: "<<student1.grade<<endl;
     return 0;
+
 }
